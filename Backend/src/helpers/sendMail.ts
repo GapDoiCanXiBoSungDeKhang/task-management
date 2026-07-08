c4dbf424c05eaea93eb9ff6a66163c5c0fd40056
LTN130925 <ltansang374@gmail.com>
2026-07-08 15:23:35 +0700

    cập nhật cấp phép port để khởi chạy nodemailer trên nền tảng render

--- Backend/src/helpers/sendMail.ts
+++ Backend/src/helpers/sendMail.ts
@@ -10,7 +10,9 @@ interface MailOptions {
 
 const sendMail = ({ from, to, subject, text, html }: MailOptions) => {
   const smtpTransport = nodemailer.createTransport({
-    service: 'Gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true bắt buộc cho port 465
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
